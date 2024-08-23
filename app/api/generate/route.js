06016fc983738d567d36cec2474c0bdb0ab312ad
Prosad Shaha <prosadshaha94@gmail.com>
2024-08-23 18:43:46 -0400

    fixing api leak

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyCzo7dA9geosPwTkhaLKy_T5WLcvY5HJL0");
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
